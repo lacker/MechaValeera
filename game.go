ba22811eb6a56a30991816a57649f4bb282ed46d
Kevin Lacker <lacker@gmail.com>
2021-08-23 15:55:54 -0700

    some games truct

--- game.go
+++ game.go
@@ -2,6 +2,25 @@ package main
 
 import "fmt"
 
+type Card int
+
+const (
+	COIN Card = iota
+	FOXY
+	MINSTREL
+	PILLAGER
+	SCABBS
+	SPIRIT
+)
+
+type Game struct {
+	board []Card // our side of the board
+	hand  []Card // our hand
+	life  int    // the opponent's life
+	mana  int    // our mana
+	storm int    // number of things played this turn
+}
+
 func main() {
 	fmt.Println("hello world")
 }

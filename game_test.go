48a0bf853b1a964a31e642413b0a7f1ca4a4fe75
Kevin Lacker <lacker@gmail.com>
2021-09-03 12:01:46 -0700

    test for finding lethal

--- game_test.go
+++ game_test.go
@@ -14,3 +14,14 @@ func TestCanPlay(t *testing.T) {
 		t.Fatalf("could not play at 0")
 	}
 }
+
+func TestCanFindLethal(t *testing.T) {
+	game := NewGame()
+	game.addCardsToHand([]Card{COIN, PILLAGER})
+	game.mana = 5
+	game.life = 1
+	ok, _, _ := game.findWin()
+	if !ok {
+		t.Fatalf("could not find win")
+	}
+}
